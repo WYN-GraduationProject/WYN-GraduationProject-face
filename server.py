513af45a62218479118dab2ee973b224e8e1d5a2
wangyaning <357208746@qq.com>
2024-03-18 21:44:04 +0800

    Initial Commit

---
+++ server.py
@@ -0,0 +1,46 @@
+import asyncio
+import logging
+
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
+from utils.tools.LoggingFormatter import LoggerManager
+from utils.tools.NacosManager import NacosManager, NacosServerUtils
+from web.face import face_api
+
+
+@asynccontextmanager
+async def app_lifespan(app: FastAPI):
+    global nacos_serverutils
+    nacos_manager = NacosManager()
+    nacos_serverutils = nacos_manager.get_server_utils("face-service", "localhost", 8001)
+    await nacos_serverutils.register_service()
+    asyncio.create_task(nacos_serverutils.beat(10))
+    try:
+        yield
+    finally:
+        await nacos_serverutils.deregister_service()
+
+
+app = FastAPI(lifespan=app_lifespan)
+
+# 允许所有来源
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 允许所有来源
+    allow_credentials=True,
+    allow_methods=["*"],  # 允许所有方法
+    allow_headers=["*"],  # 允许所有头
+)
+
+app.include_router(face_api.router)
+
+logger = LoggerManager(logger_name="server").get_logger()
+nacos_logger = logging.getLogger('nacos.client')
+nacos_logger.setLevel(logging.WARNING)
+nacos_serverutils: NacosServerUtils = None  # 定义变量以便在事件处理器中引用
+
+if __name__ == "__main__":
+    logger.info("服务启动...")
+    uvicorn.run(app, host="localhost", port=8001)
---
+++ web/face/face_api.py
@@ -0,0 +1,126 @@
+import os
+import cv2
+import numpy as np
+import uuid
+from typing import Tuple
+
+from fastapi import APIRouter, Depends, UploadFile, File
+from fastapi.responses import FileResponse
+from starlette.background import BackgroundTask
+
+from proto.video_service.video_model_pb2 import VideoFrame
+from utils.model.video import VideoModel
+from utils.tools.LoggingFormatter import LoggerManager
+from utils.tools.gRPCManager import GrpcManager
+
+router = APIRouter(
+    prefix="/api/face",
+    tags=["face"],
+    responses={404: {"description": "Not found"}},
+)
+
+logger = LoggerManager(logger_name="face_api").get_logger()
+face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+
+
+def get_grpc_manager():
+    """
+    获取gRPC管理器
+    :return: gRPC管理器
+    """
+    return GrpcManager()
+
+
+async def process_faces_in_frame(frame, face_cascade):
+    """
+    在给定帧上进行人脸检测和处理
+    :param frame: 要处理的图像帧
+    :param face_cascade: 用于人脸检测的级联分类器
+    :return: 经过处理的图像帧的字节数据
+    """
+    # 在帧上进行人脸检测
+    faces = face_cascade.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    _, img_bytes = cv2.imencode('.jpg', frame)
+    return img_bytes.tobytes()
+
+
+async def process_video(video_model: VideoModel, grpc_manager) -> str | None:
+    """
+    处理视频
+    :param video_model: 视频实体
+    :param grpc_manager: gRPC管理器
+    :return:
+    """
+    global face_cascade
+    cap = cv2.VideoCapture(video_model.path + video_model.filename)
+    video_model.fps = cap.get(cv2.CAP_PROP_FPS)
+    logger.info(f"Video FPS: {video_model.fps}")
+    if not cap.isOpened():
+        logger.error(f"打开文件失败: {video_model.path + video_model.filename}")
+        return
+    async with grpc_manager.get_stub('video_pre_service') as stub:
+        async def request_generator():
+            """
+            请求生成器
+            :return: 视频处理的请求
+            """
+            while cap.isOpened():
+                ret, frame = cap.read()
+                if not ret:
+                    break
+                _, img = cv2.imencode('.jpg', frame)
+                img_bytes = img.tobytes()
+                yield VideoFrame(data=img_bytes, is_final=False, video_id=video_model.id)
+            yield VideoFrame(is_final=True, fps=video_model.fps, video_id=video_model.id)
+
+        try:
+            response_stream = stub.ProcessVideo(request_generator())
+            async for response in response_stream:
+                response_frame = np.frombuffer(response.data, dtype=np.uint8)
+                response_frame = cv2.imdecode(response_frame, cv2.IMREAD_COLOR)
+
+                # 人脸检测
+                face_pro_img_bytes = await process_faces_in_frame(response_frame, face_cascade)
+                video_model.data.append(face_pro_img_bytes)
+        except Exception as e:
+            # gRPC服务调用失败，不进行预处理，目前返回原视频路径
+            logger.error(f"RPC预处理视频失败: {e}")
+            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            while True:
+                ret, frame = cap.read()
+                if not ret:
+                    break
+                face_pro_img_bytes = await process_faces_in_frame(frame, face_cascade)
+                video_model.data.append(face_pro_img_bytes)
+        finally:
+            await video_model.save()
+            cap.release()
+            return video_model.path + video_model.filename
+
+
+async def save_video(video: UploadFile) -> VideoModel:
+    video_id = str(uuid.uuid4())
+    video_filename = f"{video_id}.mp4"
+    video_save_path = f"video_data/original/"
+    video_model = VideoModel(video_save_path, video_filename, video_id, [], 30)
+    # 创建保存视频的目录
+    os.makedirs(video_save_path, exist_ok=True)
+    # 保存上传的视频到文件
+    filepath = os.path.join(video_save_path, video_filename)
+    with open(filepath, "wb") as f:
+        f.write(await video.read())
+    # 打印文件路径和视频文件名
+    logger.info(f"Saved video to {filepath}")
+    return video_model
+
+
+@router.post("/test")
+async def upload_video(video: UploadFile = File(...), grpc_manager: GrpcManager = Depends(get_grpc_manager)):
+    # 传递视频文件路径给处理函数
+    video_model = await save_video(video)
+    file_name = await process_video(video_model, grpc_manager)
+    response = FileResponse(file_name, media_type="video/mp4", filename="video.mp4",
+                            background=BackgroundTask(lambda: os.remove(file_name)))
+    return response
